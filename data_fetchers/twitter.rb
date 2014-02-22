b62e1917d0aaf04fcb453ea625431174ed1d0783
Jason Berlinsky <jason@barefootcoders.com>
2014-02-22 18:46:01 -0500

    Diagnostic

--- data_fetchers/twitter.rb
+++ data_fetchers/twitter.rb
@@ -4,15 +4,5 @@ require 'json'
 
 redis = Redis.new(timeout: 0)
 
-puts "Getting ready to subscribe"
-redis.subscribe('tweets') do |on|
-  puts "Loop"
-  on.subscribe do |channel, subscriptions|
-    puts "Subscribed to ##{channel} (#{subscriptions} subscriptions)"
-  end
-  on.message do |channel, msg|
-    puts msg
-    data = JSON.parse(msg)
-    puts "##{channel} - [#{data['user']}]: #{data['msg']}"
-  end
-end
+list = redis.get('tweets')
+puts list
