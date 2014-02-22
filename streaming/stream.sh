a357d309632f3b55f458d3a881d521adf9604756
Jason Berlinsky <jason@barefootcoders.com>
2014-02-22 17:40:40 -0500

    Fix stream resolution

--- streaming/stream.sh
+++ streaming/stream.sh
@@ -6,7 +6,7 @@
 # gist created by brodul
  
 INRES="1080x1440" # input resolution (The resolution of the program you want to stream!)
-OUTRES="800x720" # Output resolution (The resolution you want your stream to be at)
+OUTRES="1080x1440" # Output resolution (The resolution you want your stream to be at)
 FPS="30" # target FPS
 QUAL="medium" 
 # one of the many FFMPEG preset on (k)ubuntu found in /usr/share/ffmpeg
