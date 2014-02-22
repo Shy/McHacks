b167bf3a3be087c976b3bb2470190321a1a22b56
Jason Berlinsky <jason@barefootcoders.com>
2014-02-22 15:27:28 -0500

    Herp derp

--- lib/epp_response_handler.rb
+++ lib/epp_response_handler.rb
@@ -1,7 +1,7 @@
 require_relative 'epp_text_analyzer.rb'
 
 class EppResponseHandler
-  def handle!(text, source)
+  def self.handle!(text, source)
     FirebaseClient.add_message(
       EppTextAnalyzer.analyze_with_timestamp_and_source(text, Time.now.to_i, source)
     )
