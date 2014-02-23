635e1896ab35dfd54feff5457eedb0a11982b4ea
Jason Berlinsky <jason@barefootcoders.com>
2014-02-23 01:53:55 -0500

    Preliminary back-MMS support

--- GameBoy-Online/js/events/actions.js
+++ GameBoy-Online/js/events/actions.js
@@ -3,5 +3,15 @@ var submissions = epp.child('submissions');
 
 submissions.on('child_added', function(submissionSnapshot) {
 	var submission = submissionSnapshot.val();
-	GameBoyKeyDown(submission.action, function() {});
+	GameBoyKeyDown(submission.action, function() {
+    if (submission.sender_phone) {
+      $.post(
+        'http://everyoneplayspokemon.com:4567/back_mms',
+        {
+          recipient: submission.sender_phone,
+          canvas_dump: 'abc'
+        }
+      );
+    }
+  });
 });
--- data_fetchers/twilio.rb
+++ data_fetchers/twilio.rb
@@ -1,6 +1,26 @@
 require_relative '../lib/epp_response_handler.rb'
 
+require 'twilio-ruby'
+
+$TWILIO_SID = 'AC296e9cb4aab9b5517287d956e8114c2e'
+$TWILIO_AUTH_TOKEN = '8e0e1c7bbae2190ebe73330bed92353f'
+$TWILIO_NUMBER_MMS = '+14506007452'
+client = Twilio::REST::Client.new($TWILIO_SID, $TWILIO_AUTH_TOKEN)
+from = $TWILIO_NUMBER_MMS
+
 post '/twilio' do
   EppResponseHandler.handle!(params['Body'], 'twilio', { sender_phone: params['From']})
+  {}.to_json
+end
+
+# Send back an MMS of the game state when possible
+post '/back_mms' do
+  recipient_number = params['recipient']
+  client.account.messages.create(
+    from: from,
+    to: recipient_number,
+    body: 'Current game state',
+    media_url: params['canvas_dump']
+  )
   {}.to_json
 end
