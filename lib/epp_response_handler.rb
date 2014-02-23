74a69a520097ae148e27553918cb1e14d333120c
Jason Berlinsky <jason@barefootcoders.com>
2014-02-23 01:29:45 -0500

    Allow extra data to be sent to Firebase with Twilio requests

--- lib/epp_response_handler.rb
+++ lib/epp_response_handler.rb
@@ -2,9 +2,9 @@ require_relative 'epp_text_analyzer.rb'
 require_relative 'firebase_client.rb'
 
 class EppResponseHandler
-  def self.handle!(text, source)
+  def self.handle!(text, source, extra_data = {})
     FirebaseClient.add_message(
-      EppTextAnalyzer.analyze_with_timestamp_and_source(text, Time.now.to_i, source)
+      EppTextAnalyzer.analyze_with_timestamp_and_source(text, Time.now.to_i, source).merge(extra_data)
     )
   end
 end
