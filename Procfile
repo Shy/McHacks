57b5687fa3e4a0ba927f630f3f27dfedcb677866
Jason Berlinsky <jason@barefootcoders.com>
2014-02-22 19:01:28 -0500

    Finish moving to webhooks

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: ruby data_fetchers/server.rb
-twitter: ruby data_fetchers/twitter.rb
--- data_fetchers/twitter.rb
+++ data_fetchers/twitter.rb
@@ -1,3 +1,5 @@
+require_relative '../lib/epp_response_handler.rb'
+
 post '/twitter' do
   puts params.inspect
   puts "======="
