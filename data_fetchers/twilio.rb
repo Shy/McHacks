ed816b2fe944ca288e0c48dd7a6b210afed9b37d
Jason Berlinsky <jason@barefootcoders.com>
2014-02-23 01:36:46 -0500

    Name things better

--- data_fetchers/twilio.rb
+++ data_fetchers/twilio.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/epp_response_handler.rb'
 
 post '/twilio' do
-  EppResponseHandler.handle!(params['Body'], 'twilio', { sender: params['From']})
+  EppResponseHandler.handle!(params['Body'], 'twilio', { sender_phone: params['From']})
   {}.to_json
 end
